7ed9d8fd16d311cfa2bd346d7de18955884beb1a
Arsalan Asghar <www.arsalanasghar97@gmail.com>
2026-02-16 21:59:05 +0500

    TruthLens Project (Ai Week Version)

---
+++ env.js
@@ -0,0 +1,5 @@
+// ✅ ATTACH TO WINDOW so script.js can see it
+window.ENV = {
+    GROQ_SECRET: "d3NrdVNKb1cxRU4wNmNKaGlyb1hPRjk0WUYzYnlkR1dBcFp3VWxGRkpMZlRVeGttdzJGUF9rc2c", 
+    TAVILY_SECRET: "NVFXQnJTS3JCUWZJRURSM3ltdkhVd3NYeDhLVVRyWmstdmVkLXlsdnQ"
+};
